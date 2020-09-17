d027557a8a84b61fc27c9680e10fe99be54522b7
Matias Jalava <matias@elm.fi>
2020-09-17 15:43:47 +0300

    made ar-fragment into activity and other stuff

--- app/src/main/java/com/example/ar_furniture/MainActivity.kt
+++ app/src/main/java/com/example/ar_furniture/MainActivity.kt
@@ -2,17 +2,6 @@ package com.example.ar_furniture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MotionEvent
-import android.widget.Toast
-import com.google.ar.core.HitResult
-import com.google.ar.core.Plane
-import com.google.ar.sceneform.AnchorNode
-import com.google.ar.sceneform.HitTestResult
-import com.google.ar.sceneform.Node
-import com.google.ar.sceneform.rendering.ViewRenderable
-import com.google.ar.sceneform.ux.ArFragment
-import com.google.ar.sceneform.ux.BaseArFragment
-import com.google.ar.sceneform.ux.TransformableNode
 
 class MainActivity : AppCompatActivity(), onFurnitureSelected {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +16,7 @@ class MainActivity : AppCompatActivity(), onFurnitureSelected {
     }
 
     override fun onFurnitureSelected(furniture: Furniture, position: Int) {
-        val furnitureArFragment = FurnitureArFragment.newInstance(furniture)
+        val furnitureArFragment = FurnitureArActivity.newInstance(furniture)
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.container, furnitureArFragment, "furnitureinar")
